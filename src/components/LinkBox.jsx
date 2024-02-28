5cbf70b64a874936952012347fa5183a9028e9e4
artinmohsenpour <mohsenpour.artin@gmail.com>
2024-02-28 19:55:03 +0100

    upadated

--- src/components/LinkBox.jsx
+++ src/components/LinkBox.jsx
@@ -1,10 +1,42 @@
 import "./LinkBox.scss";
 import { original, delivery, receipt, fragile } from "../assets";
 
+const images = [
+  {
+    img: original,
+    title: "ضمانت اصالت کالا",
+  },
+  {
+    img: receipt,
+    title: "فاکتور رسمی آمازون",
+  },
+  {
+    img: fragile,
+    title: "ضمانت سلامت کالا",
+  },
+  {
+    img: delivery,
+    title: "تحویل بموقع",
+  },
+];
+
 function LinkBox() {
   return (
     <div className="linkBox__container">
-      <p>Link Box</p>
+      <div className="logo__container">
+        {images.map((img, index) => {
+          return (
+            <div key={index} className="image__container">
+              <img src={img.img} alt="logo images" />
+              <div>
+                <p>{img.title}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="link__container">Box 2</div>
+      <div className="description__container">Box 3</div>
     </div>
   );
 }
